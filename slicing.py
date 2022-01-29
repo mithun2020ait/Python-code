921a83824fa983d0455692c0bb12e0a84cb27804
Suvendu Chakrabarty <suva123chak@gmail.com>
2022-01-28 18:16:24 -0800

    new file added

---
+++ slicing.py
@@ -0,0 +1,15 @@
+s = 'abcde'
+
+print(s[1:3])
+print(s[:])
+print(s[1:])
+print(s[:4])
+
+#Steping
+print(s[0:5:2])
+
+print(s)
+
+#Negative indexing
+
+print(s[::-1])
---
+++ strings.py
@@ -0,0 +1,18 @@
+# '''
+# name = "Suvendu"
+# # paragrapg = ''' This is a 
+# # paragraph string in
+# # python. 
+# # This is for testing'''
+
+# print(paragrapg)
+# '''
+
+#String Indexing
+
+fruit = "Apple"
+
+print(fruit[-1])
+
+my_char = fruit[2]
+print(my_char)
