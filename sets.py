e3a217ba411393ca77204eab772016e7a8449c69
Suvendu Chakrabarty <suva123chak@gmail.com>
2022-01-31 18:23:19 -0800

    new file added

---
+++ sets.py
@@ -0,0 +1,24 @@
+#Set
+# a = {1,1,2,2,4,9,3}
+# print(a)
+
+# for elements in a:
+#     print(elements*2)
+
+
+#Dictionary
+marks = {'Suvendu': 45, 'Sahid':44, 'Umar': 46}
+print(marks['Suvendu'])
+
+#for loop in dictionary
+'''
+squares = {1: 1, 2: 4, 3: 9, 4: 16}
+
+for i in squares:
+    print(i, squares[i])
+'''
+
+#Updated the dictionary
+marks['Mohsin'] = 99
+marks['Suvendu'] = 50
+print(marks)
