1e7cdc6b2a7d99a59b2397a7a3329342fe56594e
Suvendu Chakrabarty <suva123chak@gmail.com>
2022-02-05 17:59:33 -0800

    new file added

---
+++ global_variable.py
@@ -0,0 +1,8 @@
+def myfunc():
+   global x  
+   x = 'fantastic'
+
+myfunc()
+
+print("python is " + x)
+
--- strings.py
+++ strings.py
@@ -26,8 +26,15 @@
 # print(c)
 
 #For loop in a string
-
+'''
 a = "abc"
 
 for my_char in a:
     print(my_char * 2, end=" ")
+'''
+
+txt = "The best things in life are free"
+if "free" not in txt:
+    print("No, 'expensive' is not  in the string.")
+else:
+    print("free is present in the string")
