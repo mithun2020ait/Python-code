0dda175cb68fb2cf2d501e540a149e90459c7c74
Suvendu Chakrabarty <suva123chak@gmail.com>
2022-02-06 21:08:57 -0800

    new file added

---
+++ factorial.py
@@ -0,0 +1,27 @@
+# def factorial (n):
+#     if(n == 1 or n == 0):
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+    
+
+# print(factorial(4))
+
+import math
+
+def factorial(n):
+    return(math.factorial(n))
+
+num = 4
+
+# print(factorial(num))
+
+a = [
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five"
+]
+
+print(len(a))
