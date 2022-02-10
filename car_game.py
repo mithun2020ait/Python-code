f459f0f7b8199d314e83e44b1fcc0ead1176a836
Suvendu Chakrabarty <suva123chak@gmail.com>
2022-02-09 22:34:21 -0800

    new file added

---
+++ car_game.py
@@ -0,0 +1,26 @@
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car is already started")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped")
+        else:
+            started = False
+            print("Car stopped...")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to quit
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry, I don't understand'")
